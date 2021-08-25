a2025523105e778effce37407170108312e7e301
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-25 19:52:21 -0300

    scanner m to cm

--- src/br/com/zup/Aula03_25_08_21_Exemplo03.java
+++ src/br/com/zup/Aula03_25_08_21_Exemplo03.java
@@ -12,13 +12,13 @@ public class Aula03_25_08_21_Exemplo03 {
         double medidaMetros;
 
         // Dando essa conversão ao usuário
-        System.out.println( " Bem Vindo ao sistema de conversão de medida" );
-        System.out.println("Insira abaixo o valor em metros que você quer transformar em centímetros");
-        medidaMetros     = calculoMetros.nextDouble();
+        System.out.println( "Faça a conversão de metros para centimetros" );
+        System.out.println("Digite o valore para ser convertido");
+        medidaMetros = calculoMetros.nextDouble();
         //Transformando os metros em centímetros
         double centimetros = medidaMetros * 100;
         // Informando ao usuário
-        System.out.println("Isso equivale a " + centimetros + " centimetros");
+        System.out.println(medidaMetros + "m equivale a " +centimetros + " cm");
 
     }
 }
