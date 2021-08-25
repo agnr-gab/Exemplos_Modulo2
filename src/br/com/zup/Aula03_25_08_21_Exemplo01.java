023d151ee9710d95902c638308f5a05b52481f6b
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-25 12:16:24 -0300

    Exercicio finalizado

--- src/br/com/zup/Aula03_25_08_21_Exemplo01.java
+++ src/br/com/zup/Aula03_25_08_21_Exemplo01.java
@@ -1,5 +1,6 @@
 package br.com.zup;
 
+//Importando o Scanner
 import java.util.Scanner;
 
 public class Aula03_25_08_21_Exemplo01 {
@@ -9,12 +10,13 @@ public class Aula03_25_08_21_Exemplo01 {
     Scanner leitorDeDados = new Scanner(System.in);
 
     //Inserção de dados do usuário
-        System.out.println("Digite seu nome, por favor");
+        System.out.println("Digite seu nome, por favor.");
         String nomeDoUsuario = leitorDeDados.next();
         System.out.println("Digite o ano do seu nascimento");
         int anoDeNascimento = leitorDeDados.nextInt();
 
-    //
+    //Mostrando os dados para o usuário
+        System.out.println("O seu nome é: " +nomeDoUsuario + " e o ano do seu nascimento é: " +anoDeNascimento);
 
     }
 
