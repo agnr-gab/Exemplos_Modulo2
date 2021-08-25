16bf2883662016c1be49d5dee9996779932e32c2
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-25 12:09:53 -0300

    Aprendendo o uso do Scanner

--- src/br/com/zup/Aula03_25_08_21_Exemplo01.java
+++ src/br/com/zup/Aula03_25_08_21_Exemplo01.java
@@ -1,7 +1,20 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Aula03_25_08_21_Exemplo01 {
     public static void main(String[] args) {
+
+    //Intaciando o meu objeto
+    Scanner leitorDeDados = new Scanner(System.in);
+
+    //Inserção de dados do usuário
+        System.out.println("Digite seu nome, por favor");
+        String nomeDoUsuario = leitorDeDados.next();
+        System.out.println("Digite o ano do seu nascimento");
+        int anoDeNascimento = leitorDeDados.nextInt();
+
+    //
 
     }
 
