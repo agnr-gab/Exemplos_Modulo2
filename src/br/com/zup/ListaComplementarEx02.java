fed81c5d402916d5c1cdad2cc3d7482812916d5b
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-26 18:02:27 -0300

    desafio da tinta

--- src/br/com/zup/ListaComplementarEx02.java
+++ src/br/com/zup/ListaComplementarEx02.java
@@ -11,22 +11,32 @@ public class ListaComplementarEx02 {
         //DESAFIO: Misture latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
         Scanner leitorTinta = new Scanner(System.in);
-        int tamanhoLocal;
-        int tinta18L = 18;
-        double tintaGalao = 3,6;
-        int valorLata18L = 80;
+        double area;
+        double quantidadeLata = 0;
+        double quantidadeGalao = 0;
+        double valorLata18L = 80;
         double valorGalaoTinta = 25;
-        double capacidadeLitro = 6;
+
 
         System.out.println("Informe o tamanho da área a ser pintada em m²: ");
-        tamanhoLocal = leitorTinta.nextInt();
+        area = leitorTinta.nextInt();
 
         // Calculo da quantidade de tinta de acordo com o tamanho do local
         // 18l de tinta = 108m² que custa 80,00 reais
         // 3.6L de tinta = 21.6m³
 
-        double quantidadeDeTintaLata = tamanhoLocal * tinta18L;
-        double quantidadeDeTintaGalao = tamanhoLocal * tintaGalao;
+        double litrosTotal = area / 6;
+        quantidadeLata = litrosTotal / 18;
+
+        System.out.println("Você usará " + quantidadeLata + " latas de linta de 18L para realizar a pintura");
+        System.out.println("O valor será de R$" + (quantidadeLata * 80) + " reais ");
+
+        quantidadeGalao = litrosTotal / 3.6;
+        System.out.println("Você usará " + quantidadeGalao + " galões de tinta de 3.6L para realizar a pintura");
+        System.out.println("O valor será de R$" + (quantidadeGalao * 25) + " reais ");
+
+        double desperdicioLata = Math.round(quantidadeLata) - quantidadeLata;
+        double desperdicioGalao = Math.round(quantidadeGalao) - quantidadeGalao;
 
 
 
